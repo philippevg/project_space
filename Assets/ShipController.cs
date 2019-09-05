f00b4ec5ca3b41f94d21bc23b74c9dfa15c72a99
Philippe <philippe.van.gucht@hotmail.com>
2019-09-05 17:19:18 +0200

    rocket makes sound

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space)) {
+            audioSource.Stop();
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
     }
 }
